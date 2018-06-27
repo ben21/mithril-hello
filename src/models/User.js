e5ea22a5ac3583336b4226c4f5a0cb6d78320e41
ben21 <bvanbever1@gmail.com>
2018-06-27 14:06:53 +0200

    Tutorial app finished

--- src/models/User.js
+++ src/models/User.js
@@ -12,6 +12,27 @@ var User = {
             User.list = result.data
         })
     },
+
+    current: {},
+    load: function(id) {
+        return m.request({
+            method: "GET",
+            url: "https://rem-rest-api.herokuapp.com/api/users/" + id,
+            withCredentials: true,
+        })
+        .then(function(result) {
+            User.current = result
+        })
+    },
+
+    save: function() {
+        return m.request({
+            method: "PUT",
+            url: "https://rem-rest-api.herokuapp.com/api/users/" + User.current.id,
+            data: User.current,
+            withCredentials: true,
+        })
+    }
 }
 
 module.exports = User
